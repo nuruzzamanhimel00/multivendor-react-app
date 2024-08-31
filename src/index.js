dad9610a6e375aced32a0b054ca5d32ac6e235d4
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-08-31 20:17:39 +0600

    react router install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+//react router
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from "./routers/index.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
